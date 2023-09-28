37cda876cc4414b4218da02f9b0b7f5f04fd7fd9
p002808 <phuccao.30012001@gmail.com>
2023-09-28 10:10:35 +0700

    research mockRestore, mockReset, mockClear

--- src/challenge-06-spy-mock-return/RandomColor.test.js
+++ src/challenge-06-spy-mock-return/RandomColor.test.js
@@ -1,10 +1,13 @@
 import RandomColor from "./RandomColor";
 import * as helper from "./helper";
 
+//sau khi sử dụng mockRestore thì sẽ bỏ ko mock nữa, cho dù các testcase dưới có sử dụng mockReturnValue như nào đi nữa cũng ko có tác dụng gì .
+//sau khi sử dụng mockReset thì sẽ reset implement của hàm mock trả về undefined, nhưng sau đso vẫn mockReturnValue được.
 describe('newColor()', () => {
   it('should return a color', () => {
     // TODO: implement test
-    jest.spyOn(helper, 'getRandomInt').mockReturnValue(0);
+    const mockGetRandomInt = jest.spyOn(helper, 'getRandomInt');
+    mockGetRandomInt.mockReturnValue(0);
     const randomColor = new RandomColor();
 
     const color = randomColor.newColor();
@@ -16,13 +19,41 @@ describe('newColor()', () => {
 describe('when calling "newColor()" multiple times', () => {
   it('should return different colors', () => {
     // TODO: implement test
-    jest.spyOn(helper, 'getRandomInt').mockReturnValueOnce(0).mockReturnValueOnce(1).mockReturnValueOnce(2);
+    const mockGetRandomInt = jest.spyOn(helper, 'getRandomInt');
+    mockGetRandomInt.mockReturnValueOnce(0);
 
     const randomColor = new RandomColor();
-    const color1 = randomColor.newColor();
+    randomColor.newColor();
+    mockGetRandomInt.mockRestore();
+    mockGetRandomInt.mockReturnValueOnce("abc");
+    //ko có tác dụng vẫn ra số random như thường. Ko còn mock return được nữa
+    console.log(helper.getRandomInt());
     const color2 = randomColor.newColor();
-    const color3 = randomColor.newColor();
 
-    expect(randomColor.generatedColors).toEqual(['blue'])
+    expect(color2).not.toBe('red')
   });
+
+  it('should return undefinded when use mockReset', ()=>{
+    const mockGetRandomInt = jest.spyOn(helper, 'getRandomInt');
+    const randomColor = new RandomColor();
+    mockGetRandomInt.mockReset();
+    //Vẫn mock return được
+    mockGetRandomInt.mockReturnValueOnce(1);
+    console.log(helper.getRandomInt());
+    const color = randomColor.newColor();
+
+    expect(color).toBe(undefined)
+  })
+
+  it('', ()=>{
+    const mockGetRandomInt = jest.spyOn(helper, 'getRandomInt');
+    const randomColor = new RandomColor();
+    mockGetRandomInt.mockClear();
+    mockGetRandomInt.mockImplementation(()=>{
+      console.log("hahah")
+      return 1;
+    })
+    randomColor.newColor();
+  })
 });
+
